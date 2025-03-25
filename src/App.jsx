f43a8cfd989ed0b7265660c378ed54a2605d9bdc
Amit9956 <amitk312002@gmail.com>
2025-03-25 18:24:22 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,55 @@ import { FaLaptopCode, FaLink, FaRegLightbulb, FaTwitter } from 'react-icons/fa'
 import { MdBookmarks } from 'react-icons/md'
 import { PiBuildingsFill } from 'react-icons/pi'
 import { CiSearch } from 'react-icons/ci'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { LuSunDim } from 'react-icons/lu'
+import axios from 'axios'
+import moment from 'moment/moment'
+
 
 function App() {
   
 
   const [color, setColor] = useState(false);
 
+  const [search, setSearch] = useState("Amit9956");
+
+  const [inputValue, setInputValue] = useState("Amit9956");
+
+  const [data, setData] = useState()
+
+
+  const fetchApi = async () => {
+
+
+    try{
+
+      const result = await axios.get(`https://api.github.com/users/${search}`)
+
+      console.log(result)
+
+      setData(result?.data)
+
+
+
+    }catch(e){
+
+      console.error(e)
+
+    }
+
+  }
+
   const handle1 = () => {
     setColor(!color);
   };
+
+  useEffect(() => {
+
+    fetchApi()
+    
+  }, [search])
+  
 
 
   return (
@@ -30,35 +68,36 @@ function App() {
           </div>
           <div className='relative '>
           <CiSearch className='absolute text-blue-600  text-2xl top-6 lg:ml-5 ' />
-          <input type='search' className={`${color ? "bg-[#1E2A47]  placeholder:text-white" : " bg-white   placeholder:text-[#4B6A9B]"} lg:px-14 px-6 py-6 rounded-2xl w-full  outline-none lg:text-xl text-sm `} name='search' placeholder='Enter a GitHub username...'/>
-          <button className='bg-blue-500 px-6 absolute right-2 top-3.5  text-white rounded-lg py-3 font-semibold'>Search</button>
+          <input type='search' className={`${color ? "bg-[#1E2A47]  placeholder:text-white" : " bg-white   placeholder:text-[#4B6A9B]"} lg:px-14 px-6 py-6 rounded-2xl w-full  outline-none lg:text-xl text-sm `} name='search' placeholder='Enter a GitHub username...'  onChange={(e) => setInputValue(e.target.value)}/>
+          <button className='bg-blue-500 px-6 absolute right-2 top-2.5  text-white rounded-lg py-3 font-semibold' onClick={() => setSearch(inputValue)}>Search</button>
           </div>
         </div>
-        <div className={`lg:h-[70vh] lg:w-[50%] ${color ? "bg-[#1E2A47] text-white" : "bg-white text-[#4b6a9b]"} lg:mx-auto mx-5 my-4 flex lg:flex-row flex-col`}>
+        <div className={`lg:h-[70vh] lg:w-[50%] ${color ? "bg-[#1E2A47] text-white" : "bg-white text-[#4b6a9b]"} lg:mx-auto mx-5 my-4 flex lg:flex-row flex-col rounded-2xl`}>
           <div className='w-[30%] h-full '>
-              <img className='rounded-full lg:h-[35%] lg:mx-auto  ml-24 my-5' src='https://avatars.githubusercontent.com/u/137944649?v=4'/>
+              <img className='rounded-full lg:h-[35%] lg:mx-auto  ml-28 md:ml-[115%] my-5' src={data?.avatar_url}/>
           </div>
           <div className={`lg:w-[70%] h-full`}>
             <div className='flex lg:flex-row flex-col items-center justify-between lg:mx-5  mx-2  lg:mt-10 '>
-              <p className={`lg:text-3xl text-4xl font-bold ${color ? "text-white" : "text-black"}`}>Amit Kumar</p>
-              <p className='lg:text-lg text-md lg:pr-5 '>Joined 20 Jan 2023</p>
+              <p className={`lg:text-3xl text-4xl font-bold ${color ? "text-white" : "text-black"}`}>{data?.name}</p>
+              <p className='lg:text-lg text-md lg:pr-5 '>{moment(data?.created_at
+              ).format("Do MMM YYYY")}</p>
             </div>
-            <a href='https://github.com/Amit9956' className='text-blue-500 mt-3 lg:mx-5 mx-32 underline'>@amit9956</a>
+            <a href={data?.html_url} className='text-blue-500 mt-3 lg:mx-5  ml-32 underline'>{data?.login}</a>
 
             <div className='lg:w-[95%] text-md my-4 lg:mx-5 mx-2'>
-            <p className='flex gap-x-4 '><FaLaptopCode className='mt-1'/>I write Code</p>
-            <p className='flex gap-x-4 '><MdBookmarks className='mt-1 text-green-500'/>Learning, coding, and exploring the digital world</p>
-            <p className='flex gap-x-4 '><FaRegLightbulb className='mt-1 text-yellow-300' />Passionate about software development, algorithms, and AI</p>
+            <p className='flex gap-x-4 '>{data?.bio}</p>
+            
             </div>
             
-            <div className={`h-[18%] w-[80%] lg:mx-auto mx-8 ${color ? "bg-[#141D2F]":"bg-[#F6F8FF]"} lg:my-10 rounded-md flex items-center justify-evenly`}>
+            <div className={`h-[18%] lg:w-[80%] w-full  lg:mx-auto  ${color ? "bg-[#141D2F]":"bg-[#F6F8FF]"} lg:my-10 rounded-md flex items-center justify-evenly`}>
                 <div className='w-[30%] pl-6'>
                 <p className=''>Respo</p>
-                <p className='text-2xl font-semibold'>7</p>
+                <p className='text-2xl font-semibold'>{data?.public_repos}</p>
                 </div>
                 <div className='w-[30%]'>
                 <p className=''>Followers</p>
-                <p className='text-2xl font-semibold'>0</p>
+                <p className='text-2xl font-semibold'>{data?.
+                  followers}</p>
                 </div>
                 <div className='w-[30%]'>
                 <p className=''>Following</p>
@@ -71,13 +110,14 @@ function App() {
                     <p className='pr-20 flex gap-x-3 text-gray-400 underline'><FaLink className='mt-1'  />Not Available</p>
                 </div>
                 <div className='flex lg:flex-row flex-col justify-between text-lg '> 
-                    <p className='lg:pl-24 flex gap-x-3 text-gray-400 underline'><FaTwitter className='mt-1 text-xl' />Not Available</p>
+                    <p className='lg:pl-24 flex gap-x-3 text-gray-400 underline'><FaTwitter className='mt-1 text-xl' />{data?.twitter_username || "Noy Available"}</p>
                     <p className='pr-20 flex gap-x-3 text-gray-400'><PiBuildingsFill className='mt-1 text-2xl' />Not Available</p>
                 </div>
               </div>
             </div>
         </div>
       </div>
+      
      
     </>
   )
